55c5e23202971f57b51b212d9247779ca0fb9bee
josephxia <15906475@qq.com>
2022-11-02 10:26:53 +0800

    feat: 修正接口

---
+++ packages/server/src/shared/dto/pagination-params.dto.ts
@@ -0,0 +1,48 @@
+import { ApiPropertyOptional } from '@nestjs/swagger';
+import { Transform } from 'class-transformer';
+import { IsNumber, IsOptional, Min } from 'class-validator';
+
+export class PaginationParamsDto {
+    @ApiPropertyOptional({
+        description: 'Optional, defaults to 100',
+        type: Number,
+    })
+    @IsNumber()
+    @IsOptional()
+    @Min(0)
+    @Transform(({ value }) => parseInt(value, 10), { toClassOnly: true })
+    limit = 100;
+
+    @ApiPropertyOptional({
+        description: 'Optional, defaults to 0',
+        type: Number,
+    })
+    @IsNumber()
+    @IsOptional()
+    @Min(0)
+    @Transform(({ value }) => parseInt(value, 10), { toClassOnly: true })
+    offset = 0;
+}
+
+
+export class PaginationParams2Dto {
+    @ApiPropertyOptional({
+        description: 'Optional, defaults to 100',
+        type: Number,
+    })
+    @IsNumber()
+    @IsOptional()
+    @Min(0)
+    @Transform(({ value }) => parseInt(value, 10), { toClassOnly: true })
+    pageSize = 100;
+
+    @ApiPropertyOptional({
+        description: 'Optional, defaults to 0',
+        type: Number,
+    })
+    @IsNumber()
+    @IsOptional()
+    @Min(0)
+    @Transform(({ value }) => parseInt(value, 10), { toClassOnly: true })
+    page = 0;
+}
