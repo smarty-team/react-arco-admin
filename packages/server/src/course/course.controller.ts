a50e7c131c98b2981a191e98e20477a94de5295d
josephxia <15906475@qq.com>
2022-11-04 09:02:32 +0800

    feat: 修改格式

--- packages/server/src/course/course.controller.ts
+++ packages/server/src/course/course.controller.ts
@@ -12,12 +12,18 @@ import {
   Query,
   Res,
   Scope,
-  UseGuards
+  UseGuards,
+  HttpStatus,
+
 } from '@nestjs/common'; import { CourseService } from './course.service';
 import { CreateCourseDto } from './dto/create-course.dto';
 import { UpdateCourseDto } from './dto/update-course.dto';
-import { ApiOperation, ApiTags } from '@nestjs/swagger';
-import { PaginationParams2Dto } from '../shared/dto/pagination-params.dto'
+import { ApiOperation, ApiTags, ApiResponse } from '@nestjs/swagger';
+
+import {
+  BaseApiErrorResponse, BaseApiResponse, SwaggerBaseApiResponse
+} from '../shared/dtos/base-api-response.dto';
+import { PaginationParams2Dto } from '../shared/dtos/pagination-params.dto'
 @ApiTags('课程')
 @Controller('course')
 export class CourseController {
@@ -26,41 +32,85 @@ export class CourseController {
   @ApiOperation({
     summary: '新增课程',
   })
+  @ApiResponse({
+    status: HttpStatus.CREATED,
+    type: SwaggerBaseApiResponse(CreateCourseDto),
+  })
+  @ApiResponse({
+    status: HttpStatus.NOT_FOUND,
+    type: BaseApiErrorResponse,
+  })
   @Post()
-  create(@Body() createCourseDto: CreateCourseDto) {
-    return this.courseService.create(createCourseDto);
+  async create(@Body() createCourseDto: CreateCourseDto) {
+    return {
+      data: await this.courseService.create(createCourseDto),
+    }
   }
 
   @ApiOperation({
     summary: '查找所有课程',
   })
+  @ApiResponse({
+    status: HttpStatus.OK,
+    type: SwaggerBaseApiResponse([CreateCourseDto]),
+  })
+  @ApiResponse({
+    status: HttpStatus.NOT_FOUND,
+    type: BaseApiErrorResponse,
+  })
   @Get()
-  findAll(
+  async findAll(
     @Query() query: PaginationParams2Dto
   ) {
     // console.log(query)
-    return this.courseService.findAll(query);
+    const { data, count } = await this.courseService.findAll(query);
+    return {
+      data,
+      mata: { total: count }
+    }
   }
 
   @ApiOperation({
     summary: '查找单个课程',
   })
+  @ApiResponse({
+    status: HttpStatus.OK,
+    type: SwaggerBaseApiResponse(CreateCourseDto),
+  })
+  @ApiResponse({
+    status: HttpStatus.NOT_FOUND,
+    type: BaseApiErrorResponse,
+  })
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.courseService.findOne(id);
+  async findOne(@Param('id') id: string) {
+    return {
+      data: await this.courseService.findOne(id)
+    }
   }
 
   @ApiOperation({
     summary: '更新单个课程',
   })
-
+  @ApiResponse({
+    status: HttpStatus.OK,
+    type: SwaggerBaseApiResponse(CreateCourseDto),
+  })
+  @ApiResponse({
+    status: HttpStatus.NOT_FOUND,
+    type: BaseApiErrorResponse,
+  })
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateCourseDto: UpdateCourseDto) {
-    return this.courseService.update(id, updateCourseDto);
+  async update(@Param('id') id: string, @Body() updateCourseDto: UpdateCourseDto) {
+    return {
+      data: await this.courseService.update(id, updateCourseDto)
+    }
   }
 
   @ApiOperation({
     summary: '删除单个课程',
+  })
+  @ApiResponse({
+    status: HttpStatus.NO_CONTENT,
   })
   @Delete(':id')
   remove(@Param('id') id: string) {
--- packages/server/src/course/course.service.ts
+++ packages/server/src/course/course.service.ts
@@ -3,7 +3,7 @@ import { CreateCourseDto } from './dto/create-course.dto';
 import { UpdateCourseDto } from './dto/update-course.dto';
 import { In, Like, Raw, MongoRepository, ObjectID } from 'typeorm';
 import { Course } from './entities/course.mongo.entity'
-import { PaginationParams2Dto } from '../shared/dto/pagination-params.dto'
+import { PaginationParams2Dto } from '../shared/dtos/pagination-params.dto'
 @Injectable()
 export class CourseService {
   constructor(
@@ -13,33 +13,38 @@ export class CourseService {
 
 
   async create(course: Course) {
-    return this.courseRepository.save(course)
+    return await this.courseRepository.save(course)
   }
 
-  async findAll({ pageSize, page }: PaginationParams2Dto): Promise<{ list: Course[], total: number }> {
+  async findAll({ pageSize, page }: PaginationParams2Dto): Promise<{ data: Course[], count: number }> {
 
-    const [list, count] = await this.courseRepository.findAndCount({
+    const [data, count] = await this.courseRepository.findAndCount({
       order: { createdAt: 'DESC' },
       skip: (page - 1) * pageSize,
       take: (pageSize * 1),
       cache: true
     })
     return {
-      list, total: count
+      data, count
     }
   }
 
-  async findOne(_id: string) {
+  async findOne(id: string) {
     // return await this.courseRepository.findOneBy({ _id: id })
-    let ret = null
-    ret = await this.courseRepository.findOneBy(_id)
-    return ret
+    console.log('id', id)
+    return await this.courseRepository.findOneBy(id)
+
+
   }
 
-  async update(_id: string, updateCourseDto: UpdateCourseDto) {
+  async update(id: string, updateCourseDto: UpdateCourseDto) {
     // const r = await this.courseRepository.findOneBy(_id)
-    console.log(_id, updateCourseDto)
-    return await this.courseRepository.update(_id, updateCourseDto)
+    // 删除时间戳
+    const update: UpdateCourseDto = {
+      name: updateCourseDto.name
+    }
+
+    return await this.courseRepository.update(id, update)
   }
 
   async remove(id: string): Promise<any> {
