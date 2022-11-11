63596f14de0ef79c6d43d3e14add3fb1712c7178
josephxia <15906475@qq.com>
2022-11-11 16:04:18 +0800

    change dto

--- packages/server/src/user/dtos/user.dto.ts
+++ packages/server/src/user/dtos/user.dto.ts
@@ -1,10 +1,21 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNotEmpty } from 'class-validator';
+import { IsNotEmpty, Matches } from 'class-validator';
+import { regMobileCN } from "@/utils/regex.util";
+
 export class CreateUserDto {
 
-    @ApiProperty({ example: 'cookie' })
+    /**
+        * 手机号（系统唯一）
+       */
+    @Matches(regMobileCN, { message: '请输入正确手机号' })
+    @IsNotEmpty({ message: '请输入手机号' })
+    @ApiProperty({ example: '13611177421' })
+    readonly mobile: string;
+
+
+    @ApiProperty({ example: '然叔' })
     @IsNotEmpty()
-    name: string;
+    nickname: string;
 
     @ApiProperty({ example: '123456' })
     @IsNotEmpty()
