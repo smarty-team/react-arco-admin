f492c58e244357eb972b4408cbcf2569b6a6731d
josephxia <15906475@qq.com>
2022-11-19 11:26:03 +0800

    update

--- packages/server/src/shared/configs/module-options.ts
+++ packages/server/src/shared/configs/module-options.ts
@@ -1,4 +1,4 @@
-import * as Joi from '@hapi/joi';
+// import * as Joi from '@hapi/joi';
 import { ConfigModuleOptions } from '@nestjs/config/dist/interfaces';
 
 import configuration from './configuration';
@@ -6,19 +6,19 @@ import configuration from './configuration';
 export const configModuleOptions: ConfigModuleOptions = {
   envFilePath: '.env',
   load: [configuration],
-  validationSchema: Joi.object({
-    APP_ENV: Joi.string()
-      .valid('development', 'production', 'test')
-      .default('development'),
-    APP_PORT: Joi.number().required(),
-    DB_URL: Joi.string().required(),
-    DB_NAME: Joi.string().required(),
-    DB_USER: Joi.string().required(),
-    DB_PASS: Joi.string().required(),
-    JWT_PUBLIC_KEY_BASE64: Joi.string().required(),
-    JWT_PRIVATE_KEY_BASE64: Joi.string().required(),
-    JWT_ACCESS_TOKEN_EXP_IN_SEC: Joi.number().required(),
-    JWT_REFRESH_TOKEN_EXP_IN_SEC: Joi.number().required(),
-    DEFAULT_ADMIN_USER_PASSWORD: Joi.string().required(),
-  }),
+  // validationSchema: Joi.object({
+  //   APP_ENV: Joi.string()
+  //     .valid('development', 'production', 'test')
+  //     .default('development'),
+  //   APP_PORT: Joi.number().required(),
+  //   DB_URL: Joi.string().required(),
+  //   DB_NAME: Joi.string().required(),
+  //   DB_USER: Joi.string().required(),
+  //   DB_PASS: Joi.string().required(),
+  //   JWT_PUBLIC_KEY_BASE64: Joi.string().required(),
+  //   JWT_PRIVATE_KEY_BASE64: Joi.string().required(),
+  //   JWT_ACCESS_TOKEN_EXP_IN_SEC: Joi.number().required(),
+  //   JWT_REFRESH_TOKEN_EXP_IN_SEC: Joi.number().required(),
+  //   DEFAULT_ADMIN_USER_PASSWORD: Joi.string().required(),
+  // }),
 };
