916a5016e203945c901413da7fd302b410572dfc
57code <yt0379@sohu.com>
2022-11-25 16:11:21 +0800

    cms init

--- packages/server/src/user/dtos/role.dto.ts
+++ packages/server/src/user/dtos/role.dto.ts
@@ -1,16 +1,21 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, Matches } from 'class-validator';
-import { regMobileCN } from "@/shared/utils/regex.util";
-import { IdDTO } from '@/shared/dtos/id.dto'
+import { regMobileCN } from '@/shared/utils/regex.util';
+import { IdDTO } from '@/shared/dtos/id.dto';
 
 export class CreateRoleDto extends IdDTO {
+  @ApiProperty({ example: 'admin' })
+  @IsNotEmpty()
+  name: string;
 
-    @ApiProperty({ example: 'admin' })
-    @IsNotEmpty()
-    name: string;
-
-    @ApiProperty({ example: [{ name: '菜单一', permissions: ['*'] }] })
-    @IsNotEmpty()
-    permissions: object;
-
+  @ApiProperty({
+    example: {
+      'dashboard/workplace': ['write', 'read'],
+      user: ['read', 'write'],
+      course: ['write', 'read'],
+      role: ['read', 'write'],
+    },
+  })
+  @IsNotEmpty()
+  permissions: object;
 }
