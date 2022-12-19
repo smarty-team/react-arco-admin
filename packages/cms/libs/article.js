442f7affc8d7a1b22537f3f5377ca915ac32daf4
57code <yt0379@sohu.com>
2022-12-19 18:55:06 +0800

    使用环境变量

--- packages/cms/libs/article.js
+++ packages/cms/libs/article.js
@@ -1,6 +1,6 @@
 // 获取菜单数据
 export function useArticle(id) {
-  return fetch("http://localhost:3000/article/" + id)
+  return fetch(process.env.host + "/article/" + id)
     .then((res) => res.json())
     .then((json) => json.data);
 }
