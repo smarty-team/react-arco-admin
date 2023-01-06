77f0c929e8373d3d13a5ea7dc90e551a1d386dfd
57code <yt0379@sohu.com>
2023-01-06 09:47:32 +0800

    打包相对地址

--- packages/admin/vite.config.ts
+++ packages/admin/vite.config.ts
@@ -6,6 +6,7 @@ import setting from './src/settings.json';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
   resolve: {
     alias: [{ find: '@', replacement: '/src' }],
   },
