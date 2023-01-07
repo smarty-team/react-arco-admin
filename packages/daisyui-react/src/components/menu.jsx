287a9a71cab27f404b7ea17580a2a839c61bb352
josephxia <15906475@qq.com>
2023-01-07 11:14:23 +0800

    ui: add loading

--- packages/daisyui-react/src/components/menu.jsx
+++ packages/daisyui-react/src/components/menu.jsx
@@ -9,18 +9,10 @@ export default function Menu() {
           class="flex-0 btn btn-ghost px-2"
         >
           <div class="font-title text-primary inline-flex text-lg transition-all duration-200 md:text-3xl">
-            <span class="lowercase">daisy</span>{" "}
-            <span class="text-base-content uppercase">UI</span>
+            <span class="lowercase">🚌 前端</span>{" "}
+            <span class="text-base-content uppercase">大班车</span>
           </div>
         </a>{" "}
-        <a
-          href="/docs/changelog"
-          class="link link-hover font-mono text-xs text-opacity-50"
-        >
-          <div data-tip="Changelog" class="tooltip tooltip-bottom">
-            2.43.2
-          </div>
-        </a>
       </div>
       <div class="h-4"></div>
       <ul className="menu menu-compact flex flex-col p-0 px-4">
--- packages/daisyui-react/src/pages/admin.jsx
+++ packages/daisyui-react/src/pages/admin.jsx
@@ -2,7 +2,15 @@ import Menu from "../components/menu";
 import NavBar from "../components/content/navbar";
 import Content from "../components/content/content";
 import Footer from "../components/content/footer";
+import Loading from "../components/content/loading";
+import { useState } from "react";
 export default function Admin() {
+  const [display, setDispllay] = useState(true);
+
+  setTimeout(() => {
+    setDispllay(false);
+    console.log("GGG", display);
+  }, 1000);
   return (
     <div className="bg-base-100 drawer drawer-mobile">
       <input id="my-drawer" type="checkbox" className="drawer-toggle" />
@@ -15,6 +23,11 @@ export default function Admin() {
         <label htmlFor="my-drawer" className="drawer-overlay"></label>
         <Menu></Menu>
       </div>
+
+      <Loading display={display}></Loading>
+
+      {/* Put this part before </body> tag */}
+      <input type="checkbox" id="my-modal" className="modal-toggle" />
     </div>
   );
 }
