60bfa144fbf760013189c9bc1731c0edd774f087
josephxia <15906475@qq.com>
2023-01-19 10:04:20 +0800

    change ui

--- packages/ui/src/pages/index.jsx
+++ packages/ui/src/pages/index.jsx
@@ -58,12 +58,12 @@ export function Index() {
         <div className="hero min-h-screen bg-base-200">
           <div className="hero-content text-center">
             <div className="max-w-md">
-              {/* <h1 className="text-5xl font-bold">Hello there</h1>
+              <h1 className="text-5xl font-bold">Hello there</h1>
               <p className="py-6">
                 Provident cupiditate voluptatem et in. Quaerat fugiat ut
                 assumenda excepturi exercitationem quasi. In deleniti eaque aut
                 repudiandae et a id nisi.
-              </p> */}
+              </p>
 
               {collapse}
 
