5c4b85e7cb085be29568f3d7c04dc64c26c56747
josephxia <15906475@qq.com>
2023-03-01 00:13:34 +0800

    ci: del cms-old

--- packages/cms-old/tailwind.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  darkMode: ['class', '[data-theme="dark"]'],
-  content: ['./pages/**/*.{js,ts,jsx,tsx}'],
-  plugins: [require('@tailwindcss/typography'), require('daisyui')],
-};
