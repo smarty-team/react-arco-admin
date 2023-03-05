e46dcac5696e7444974a43b0370a5317120e7934
57code <yt0379@sohu.com>
2023-03-05 15:03:58 +0800

    a

--- packages/react-arco/vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { vitePluginForArco } from '@arco-plugins/vite-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), vitePluginForArco()],
-})
