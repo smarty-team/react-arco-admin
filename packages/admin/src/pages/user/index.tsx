a1cae9213f736851af486663a6c2abfce443ff72
57code <yt0379@sohu.com>
2023-03-06 11:18:38 +0800

    user page

--- packages/admin/src/pages/user/index.tsx
+++ packages/admin/src/pages/user/index.tsx
@@ -1,7 +1,21 @@
+import { Button, Card, Space, Typography } from '@arco-design/web-react';
 import React from 'react';
+
+const { Title, Text } = Typography;
 
 export default function UserPage() {
   return (
-    <div>用户管理页</div>
-  )
+    // 容器
+    <Card>
+      {/* 标题 */}
+      <Title heading={6}>用户管理</Title>
+
+      {/* 操作按钮 */}
+      <Space direction="vertical">
+        <Button>新增</Button>
+        {/* 数据表格 */}
+        <Text>用户数据</Text>
+      </Space>
+    </Card>
+  );
 }
