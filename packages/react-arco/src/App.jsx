604759690618cc8512fda1c4a4e18bccf662fd27
57code <yt0379@sohu.com>
2023-03-06 11:12:45 +0800

    layout

--- packages/react-arco/src/App.jsx
+++ packages/react-arco/src/App.jsx
@@ -1,74 +1,29 @@
-import { Grid } from '@arco-design/web-react';
+import "./App.css";
+import { Button, Icon, Layout, Space } from "@arco-design/web-react";
 
-// 栅格：
-// Row 行
-// Col 列
-const Row = Grid.Row;
-const Col = Grid.Col;
+// Layout：
+const Header = Layout.Header;
+const Content = Layout.Content;
+const Sider = Layout.Sider;
+const Footer = Layout.Footer;
 
-const App = () => {
+function App() {
   return (
-    <div style={{ width: '100%' }} className='grid-demo-background'>
-      <Row className='grid-demo' style={{ marginBottom: 16 }}>
-        <Col span={24}>
-          <div>24 - 100%</div>
-        </Col>
-      </Row>
-      <Row className='grid-demo' style={{ marginBottom: 16 }}>
-        <Col span={12}>
-          <div>12 - 50%</div>
-        </Col>
-        <Col span={12}>
-          <div>12 - 50%</div>
-        </Col>
-      </Row>
-      <Row className='grid-demo' style={{ marginBottom: 16 }}>
-        <Col span={8}>
-          <div>8 - 33.33%</div>
-        </Col>
-        <Col span={8}>
-          <div>8 - 33.33%</div>
-        </Col>
-        <Col span={8}>
-          <div>8 - 33.33%</div>
-        </Col>
-      </Row>
-      <Row className='grid-demo' style={{ marginBottom: 16 }}>
-        <Col span={6}>
-          <div>6 - 25%</div>
-        </Col>
-        <Col span={6}>
-          <div>6 - 25%</div>
-        </Col>
-        <Col span={6}>
-          <div>6 - 25%</div>
-        </Col>
-        <Col span={6}>
-          <div>6 - 25%</div>
-        </Col>
-      </Row>
-      <Row className='grid-demo'>
-        <Col span={4}>
-          <div>4 - 16.66%</div>
-        </Col>
-        <Col span={4}>
-          <div>4 - 16.66%</div>
-        </Col>
-        <Col span={4}>
-          <div>4 - 16.66%</div>
-        </Col>
-        <Col span={4}>
-          <div>4 - 16.66%</div>
-        </Col>
-        <Col span={4}>
-          <div>4 - 16.66%</div>
-        </Col>
-        <Col span={4}>
-          <div>4 - 16.66%</div>
-        </Col>
-      </Row>
-    </div>
+    <Layout style={{ height: "400px" }}>
+      <Header>
+        <Space>
+          <Button type="primary">Item1</Button>
+          <Button type="primary">Item2</Button>
+          <Button type="primary">Item3</Button>
+          <Button type="primary">Item4</Button>
+        </Space>
+      </Header>
+      <Layout>
+        <Sider>Sider</Sider>
+        <Content>Content</Content>
+      </Layout>
+      <Footer>Footer</Footer>
+    </Layout>
   );
-};
-
+}
 export default App;
