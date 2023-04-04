0f9509bb26119021092d1a6c5ea8ca73c078a76e
josephxia <15906475@qq.com>
2023-04-04 17:45:01 +0800

    fix: 解决校验问题

--- packages/server/src/user/dtos/login.dto.ts
+++ packages/server/src/user/dtos/login.dto.ts
@@ -7,7 +7,7 @@ export class LoginDTO {
   /**
    * 手机号（系统唯一）
    */
-  @Matches(regMobileCN, { message: '请输入正确手机号' })
+  // @Matches(regMobileCN, { message: '请输入正确手机号' })
   @IsNotEmpty({ message: '请输入手机号' })
   @ApiProperty({ example: '18888888888' })
   readonly phoneNumber: string;
