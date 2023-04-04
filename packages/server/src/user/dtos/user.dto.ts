f564ee472868432d3d5b9567c2ef81d865aad859
josephxia <15906475@qq.com>
2023-04-04 17:45:17 +0800

    fix: 解决校验bug

--- packages/server/src/user/dtos/user.dto.ts
+++ packages/server/src/user/dtos/user.dto.ts
@@ -7,7 +7,7 @@ export class CreateUserDto {
     /**
     * 手机号（系统唯一）
     */
-    @Matches(regMobileCN, { message: '请输入正确手机号' })
+    // @Matches(regMobileCN, { message: '请输入正确手机号' })
     @IsNotEmpty({ message: '请输入手机号' })
     @ApiProperty({ example: '13611177421' })
     readonly phoneNumber?: string;
