32ccffb966fb5862e907036b74997a6b7a4bb9c8
josephxia <15906475@qq.com>
2023-04-23 10:31:20 +0800

    update

--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -30,10 +30,10 @@ async function bootstrap() {
     }
   }))
 
-  app.use(rateLimit({
-    windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 1000, // limit each IP to 100 requests per windowMs
-  }),)
+  // app.use(rateLimit({
+  //   windowMs: 15 * 60 * 1000, // 15 minutes
+  //   max: 1000, // limit each IP to 100 requests per windowMs
+  // }),)
 
   app.useGlobalPipes(new ValidationPipe({
     forbidUnknownValues: false
